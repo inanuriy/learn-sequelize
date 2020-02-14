ddae792e057a3c267e057a9ec8f099d9be89c833
Ina Nuri Yohari <inanuriyohari@gmail.com>
2020-02-14 13:49:51 +0700

    manipulate data done

--- routes/todos/controller.js
+++ routes/todos/controller.js
@@ -1,16 +1,71 @@
 const { todos } = require("../../models");
 
 module.exports = {
-    getAll: async (req,res) => {
-        try {
-            const result = await todos.findAll()
+  getAll: async (req, res) => {
+    try {
+      const result = await todos.findAll();
 
-            res.status(200).send({
-                message: "Show all todos data",
-                data:result
-            });
-        } catch (error) {
-            console.log(error)
-        }
+      res.status(200).send({
+        message: "Show all todos data",
+        data: result
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+
+  getById: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await todos.findOne({ where: { id: id } });
+
+      res.status(200).send({
+        message: `Show data id ${id}`,
+        data: result
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  addData: async (req, res) => {
+    try {
+      const result = await todos.create(req.body);
+
+      res.status(200).send({
+        message: `Todo is successfully added`,
+        data: result
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  updateData: async (req, res) => {
+    try {
+      const {id} = req.params;
+      const result = await todos.update(req.body, { where: {id} });
+
+      res.status(200).send({
+        message: `Todo successfully updated`,
+        data: result
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  deleteData: async (req, res) => {
+    try {
+      const {id} = req.params;
+      const result = await todos.destroy({ where: {id} });
+
+      res.status(200).send({
+        message: `Todo successfully deleted`,
+        data: result
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
--- routes/todos/index.js
+++ routes/todos/index.js
@@ -2,5 +2,9 @@ const express = require("express");
 const router = express.Router()
 
 router.get("/", require("./controller").getAll);
+router.get("/:id", require("./controller").getById);
+router.post("/", require("./controller").addData);
+router.put("/:id", require("./controller").updateData);
+router.delete("/:id", require("./controller").deleteData);
 
 module.exports = router;
